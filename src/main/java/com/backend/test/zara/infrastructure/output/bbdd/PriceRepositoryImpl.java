930409acff5466ed05448ba968863fd415bf6fa1
Suso <susinodevelop@gmail.com>
2024-01-28 22:28:03 +0100

    Añade implementacion del repositorio jdbc

--- src/main/java/com/backend/test/zara/infrastructure/output/bbdd/PriceRepositoryImpl.java
+++ src/main/java/com/backend/test/zara/infrastructure/output/bbdd/PriceRepositoryImpl.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Repository;
 
+import java.sql.Timestamp;
 import java.time.LocalDateTime;
 import java.util.*;
 
@@ -27,10 +28,31 @@ public class PriceRepositoryImpl implements PriceRepository {
         this.priceRowMapper = priceRowMapper;
     }
 
+    /**
+     * Consulta en la base de datos los datos de los precios para una fecha, producto y cadena dados.
+     *
+     * @param date      Fecha de aplicación.
+     * @param productId Identificador de producto.
+     * @param brandId   Identificador de cadena.
+     * @return Todos los precios que cumplan los criterios establecidos.
+     */
     @Override
     public Collection<PriceCo> getPrices(LocalDateTime date, String productId, String brandId) {
-        //TODO to implement
-        return Collections.emptyList();
+        log.info("Consultando a BBDD los precios. Date: {}. Product Id: {}. Brand Id: {}", date, productId, brandId);
+        String sql = "SELECT BRAND.NAME, PRICES.START_DATE, PRICES.END_DATE, PRICES.PRICE_LIST, PRICES.PRODUCT_ID, " +
+                "PRICES.PRIORITY, PRICES.PRICE, PRICES.CURR " +
+                "FROM PRICES " +
+                "INNER JOIN BRAND ON PRICES.BRAND_ID = BRAND.BRAND_ID " +
+                "WHERE PRICES.BRAND_ID = :brandId " +
+                "AND PRICES.PRODUCT_ID = :productId " +
+                "AND :date BETWEEN PRICES.START_DATE AND PRICES.END_DATE";
+
+        Map<String, Object> parameters = new HashMap<>();
+        parameters.put("date", Timestamp.valueOf(date));
+        parameters.put("productId", productId);
+        parameters.put("brandId", brandId);
+
+        return jdbcTemplate.query(sql, parameters, priceRowMapper);
     }
 
 }
